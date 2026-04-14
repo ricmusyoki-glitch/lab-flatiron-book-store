df896c5d88778b22099d158f2fea516d75bc7199
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-04-14 20:38:20 +0300

    my work

--- index.js
+++ index.js
@@ -43,5 +43,27 @@ const bookStore = {
     ]
 }
 
-// Write your code here!
+const bookStoreTitle = document.querySelector('#header')
+bookStoreTitle.textContent = bookStore.name
 
+const bookList = document.querySelector('#book-list')
+
+bookStore.books.forEach((book) => {
+    const bookContainer = document.createElement('li')
+    const bookTitle = document.createElement('h3')
+    const bookAuthor = document.createElement('p')
+    const bookImage = document.createElement('img')
+
+    bookTitle.textContent = book.title
+    bookAuthor.textContent = book.author
+    bookImage.src = book.imageUrl
+
+    bookContainer.append(bookTitle, bookAuthor, bookImage)
+    bookList.append(bookContainer)
+})
+
+const deleteThis = document.querySelector('#delete-this')
+
+if (deleteThis) {
+    deleteThis.remove()
+}
